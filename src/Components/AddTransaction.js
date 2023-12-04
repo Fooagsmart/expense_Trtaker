7cbbea7297a5a5c5a884bfd35e44c69452fdf241
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-12-04 19:59:58 +0100

    removed unwanted files

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -4,14 +4,14 @@ const AddTransaction = () => {
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form id='form'>
+      <form>
         <div className='form-control'>
-            <label for="text" />
-                <input type='text' id='text' placeholder='Enter text...' />
+            <label htmlFor="text" />
+                <input type='text' placeholder='Enter text...' />
         </div>
         <div className='form-control'>
-            <label for="amount">Amount <br />(negative - expense, positive - income)</label>
-                <input type='number' id='amount' placeholder='Enter amount...' />
+            <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
+                <input type='number' placeholder='Enter amount...' />
         </div>
       </form>
     </div>
