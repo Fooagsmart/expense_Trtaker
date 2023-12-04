d968f84bc3bb80d1647120620789f32646986c4c
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-12-04 19:23:59 +0100

    Balance and expenses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Balance from './Components/Balance';
+import IncomeExpenses from './Components/IncomeExpenses';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
