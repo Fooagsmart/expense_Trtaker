fb8c48e2288d20a5e764ae2193189e55301797dc
Oluwaseun <144266502+Fooagsmart@users.noreply.github.com>
2023-12-12 20:59:27 +0100

    wrapper

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import IncomeExpenses from './Components/IncomeExpenses';
 import TransactionList from './Components/TransactionList';
 import AddTransaction from './Components/AddTransaction';
 
+import { GlobalProvider } from './Context/GlobalContext';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className='container'>
         <Balance />
@@ -16,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
